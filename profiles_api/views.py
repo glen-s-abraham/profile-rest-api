0cbcd545b4394905b7b4ab58a0b7ab4f1e508412
glen-s-abraham <glenprojects666@gmail.com>
2020-08-08 17:47:19 +0530

    Private feed and user only update added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from profiles_api import serializers
 from profiles_api import models
@@ -108,6 +109,10 @@ class UserProfilesFeedViewset(viewsets.ModelViewSet):
 	serializer_class=serializers.UserProfilesFeedSerializer
 	authentication_classes=(TokenAuthentication,)
 	queryset=models.UserProfilesFeed.objects.all()
+	permission_classes=(
+		permissions.UpdateOwnStatus,
+		IsAuthenticatedOrReadOnly
+		)
 
 	def perform_create(self,serializer):
 		"""adds to user_profile field current user"""
